dcc62b6cb6c296d59ac8617f655e193a2d766bf6
blake.good <blake.good@MU-C1-08.mullen.pvt.k12.co.us>
2018-08-27 08:58:13 -0600

    Reformatted all the code.

--- For Loops/src/ForLoops.java
+++ For Loops/src/ForLoops.java
@@ -10,42 +10,42 @@ public class ForLoops
 				challengeFour();
 
 			}
-		
+
 		public static void challengeOne()
-		{
-			for (int i = 1; i < 6; i++)
-				{
-					System.out.println(i);
-				}
-		}
-		
-		public static void challengeTwo()
-		{
-			for (int i = 2; i < 11; i = i + 2)
-				{
-					System.out.println( i + ". I Love Mullen!");	
-				}
-			
-		}
-		
-		public static void challengeThree()
-		{
-			for (int i = 1; i < 6; i++)
-				{
-					System.out.print(i);
-				}
-		}
-		
-		public static void challengeFour()
-		{
-			for (int i =10; i > 0; i--)
 			{
-				System.out.println(i);
-				if (i == 1)
+				for (int i = 1; i < 6; i++)
 					{
-						System.out.println("Liftoff!");
+						System.out.println(i);
 					}
 			}
-		}
+
+		public static void challengeTwo()
+			{
+				for (int i = 2; i < 11; i = i + 2)
+					{
+						System.out.println(i + ". I Love Mullen!");
+					}
+
+			}
+
+		public static void challengeThree()
+			{
+				for (int i = 1; i < 6; i++)
+					{
+						System.out.print(i);
+					}
+			}
+
+		public static void challengeFour()
+			{
+				for (int i = 10; i > 0; i--)
+					{
+						System.out.println(i);
+						if (i == 1)
+							{
+								System.out.println("Liftoff!");
+							}
+					}
+			}
 
 	}
