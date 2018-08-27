1895ffc4e5eb898915114d4205475cdedff2f8e5
blake.good <blake.good@MU-C1-08.mullen.pvt.k12.co.us>
2018-08-27 08:56:27 -0600

    I finished all of the challenges.

--- For Loops/src/ForLoops.java
+++ For Loops/src/ForLoops.java
@@ -21,13 +21,19 @@ public class ForLoops
 		
 		public static void challengeTwo()
 		{
-				
+			for (int i = 2; i < 11; i = i + 2)
+				{
+					System.out.println( i + ". I Love Mullen!");	
+				}
 			
 		}
 		
 		public static void challengeThree()
 		{
-			
+			for (int i = 1; i < 6; i++)
+				{
+					System.out.print(i);
+				}
 		}
 		
 		public static void challengeFour()
@@ -37,7 +43,7 @@ public class ForLoops
 				System.out.println(i);
 				if (i == 1)
 					{
-						
+						System.out.println("Liftoff!");
 					}
 			}
 		}
