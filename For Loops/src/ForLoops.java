8adcf6cf88c8407ce557477b60cb17b4e0ff9c74
blake.good <blake.good@MU-C1-08.mullen.pvt.k12.co.us>
2018-08-27 08:45:40 -0600

    finished one, working on 4

---
+++ For Loops/src/ForLoops.java
@@ -0,0 +1,45 @@
+
+public class ForLoops
+	{
+
+		public static void main(String[] args)
+			{
+				challengeOne();
+				challengeTwo();
+				challengeThree();
+				challengeFour();
+
+			}
+		
+		public static void challengeOne()
+		{
+			for (int i = 1; i < 6; i++)
+				{
+					System.out.println(i);
+				}
+		}
+		
+		public static void challengeTwo()
+		{
+				
+			
+		}
+		
+		public static void challengeThree()
+		{
+			
+		}
+		
+		public static void challengeFour()
+		{
+			for (int i =10; i > 0; i--)
+			{
+				System.out.println(i);
+				if (i == 1)
+					{
+						
+					}
+			}
+		}
+
+	}
